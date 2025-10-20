64bc80e6009ff32f31ddad49d054579e848ffd17
dev-jaime <jaime.mendes.ixia@gmail.com>
2025-10-20 01:55:51 +0000

    Auto commit de alterações 2025-10-20 01:55:51

--- scripts/deploy_firebase.sh
+++ scripts/deploy_firebase.sh
@@ -2,7 +2,7 @@
 
 # Script para deploy rápido no Firebase Hosting
 echo ""
-echo "=== Iniciando deploy no Firebase Hosting..."
+echo "=> Iniciando deploy no Firebase Hosting..."
 npx firebase deploy --only hosting
 echo ""
 echo -e "\e[32m ✔  Firebase Deploy concluído!\e[0m"
